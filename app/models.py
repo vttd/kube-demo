93641cddf9a949cc57ab0c78fd6ba08c559dad45
Trent Deal <tdeal@viatechsystems.com>
2020-01-03 07:47:11 -0500

    Create Kube model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Kube(models.Model):
+    class Colors(models.TextChoices):
+        BLACK = 'black', 'Black'
+        RED = 'red', 'Red'
+        GREEN = 'green', 'Green'
+        BLUE = 'blue', 'Blue'
+
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    color = models.CharField(
+        max_length=255,
+        choices=Colors.choices,
+        default=Colors.BLACK
+    )
