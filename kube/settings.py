841f2be031618480a92008aeb233154d87e21ea7
Trent Deal <tdeal@viatechsystems.com>
2020-01-03 12:45:06 -0500

    Change POSTGRES_NAME to POSTGRES_DB

--- kube/settings.py
+++ kube/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'kube.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME', 'kube'),
+        'NAME': os.environ.get('POSTGRES_DB', 'kube'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('POSTGRES_SERVICE_HOST', 'postgres'),
