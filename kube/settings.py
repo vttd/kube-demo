5450b3b26278a19001c4a9fe99b51eba6b8f2536
Trent Deal <tdeal@viatechsystems.com>
2020-01-03 10:37:39 -0500

    Use postgresql for default database

--- kube/settings.py
+++ kube/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'kube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME', 'kube'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_SERVICE_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_SERVICE_PORT', 5432)
     }
 }
 
