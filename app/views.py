c048f5eca56592ad379231645272b7d80123a589
Trent Deal <tdeal@viatechsystems.com>
2020-01-03 08:07:30 -0500

    Make index.html render all the kubes in the database

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Kube
+
 
 def index(request):
-    return render(request, 'app/index.html', {})
+    kubes = Kube.objects.all()
+
+    return render(request, 'app/index.html', {
+        'kubes': kubes
+    })
